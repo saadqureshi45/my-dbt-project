9ce1cfb36273f200db67ca51818b158d5050cd9c
saadqureshi45 <44566697+saadqureshi45@users.noreply.github.com>
2026-06-12 22:17:10 +0500

    Added incremental orders model

--- models/incremental_model_examples/trg_orders.sql
+++ models/incremental_model_examples/trg_orders.sql
@@ -1,22 +1,12 @@
 {{
     config(
         materialized='incremental',
-        unique_key='ORDER_ID',
-        incremental_strategy='merge'
+     
     )
 }}
 
 
-Merge into 
+
 SELECT *
 FROM DBT_DB.PROD.ORDERS
 
-{% if is_incremental() %}
-
-WHERE UPDATED_AT >
-(
-    SELECT MAX(UPDATED_AT)
-    FROM {{ this }}
-)
-
-{% endif %}
